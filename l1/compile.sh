c588536bd8a605ee6a8f302b0cb528f739e23a6f
29121970t <29121970tttt@gmail.com>
2025-09-10 22:59:16 +0300

    windows compilation suppor

--- l1/compile.sh
+++ l1/compile.sh
@@ -1,4 +1,12 @@
 BASEDIR=$(dirname "$0")
-# mkdir $BASEDIR/build
-# cmake -DCMAKE_BUILD_TYPE=DEBUG -S $BASEDIR -B $BASEDIR/build
-make -C $BASEDIR/build
+mkdir $BASEDIR/build
+cmake -S $BASEDIR -B $BASEDIR/build
+cmake --build $BASEDIR/build -j $(grep -c ^processor /proc/cpuinfo)
+for arg in "$@"; do
+    if [ "$arg" = "-w" ]; then 
+        mkdir $BASEDIR/buildW
+        cmake -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_TOOLCHAIN_FILE=./cmake/TWin.cmake -S $BASEDIR -B $BASEDIR/buildW
+        cmake --build $BASEDIR/buildW -j $(grep -c ^processor /proc/cpuinfo)
+    fi;
+done
+
