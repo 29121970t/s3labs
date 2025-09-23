d9b9663bd38d7dbe3c287d5d1507ad75eb1ca61e
29121970t <29121970tttt@gmail.com>
2025-09-23 23:50:57 +0300

    l5 fixes 3

--- l5/include/vector.hh
+++ l5/include/vector.hh
@@ -53,25 +53,6 @@ class Vector {
         if (size_ == 0) throw std::invalid_argument("empty initializer_list");
         std::ranges::copy(list, data_.get());
     }
-    Vector(const Vector &other) : size_{other.size_}, used_{size_}, data_{std::make_unique<T[]>(size_)} {
-        copy(other.data_.get(), other.data_.get() + size_, data_.get());
-    }
-    Vector(Vector &&other) noexcept : size_{other.size_}, used_{size_}, data_{std::move(other.data_)} {};
-
-    Vector &operator=(const Vector &other) {
-        size_ = other.size_;
-        used_ = other.used_;
-        data_ = std::move(std::make_unique_for_overwrite<T[]>(size_));
-        std::copy(other.data_.get(), other.data_.get() + size_, data_.get());
-        return *this;
-    };
-
-    Vector &operator=(Vector &&other) noexcept {
-        size_ = other.size_;
-        used_ = other.used_;
-        data_ = std::move(other.data_);
-        return *this;
-    };
 
     Vector &operator=(const std::initializer_list<T> &&list) {
         if (list.size() == 0) throw std::invalid_argument("empty initializer_list");
