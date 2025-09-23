5f9912401284802ffa490b9a2b8d1c02c4ee9928
29121970t <29121970tttt@gmail.com>
2025-09-23 09:22:17 +0300

    duplication fix

--- l3/src/main.cc
+++ l3/src/main.cc
@@ -24,7 +24,7 @@ int main(void) {
     unsigned int response;
     do {
         printMainScreen();
-        readT(response, ">", [](unsigned int num) { return num > 0 && num <= 4; });
+        readT(response, ">", [](unsigned int number) { return number > 0 && number <= 4; });
         cout << "\x{1B}[2J\x{1B}[H\n";
     } while (actions[response - 1]());
 
